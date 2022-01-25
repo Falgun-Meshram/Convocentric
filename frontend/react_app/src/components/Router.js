10bd3d1d2a811c75b95365519c976957910433f4
Falgun Meshram <falgunmeshram@Falguns-MacBook-Pro.local>
2022-01-25 18:27:31 -0500

    Added Not found page

--- frontend/react_app/src/components/Router.js
+++ frontend/react_app/src/components/Router.js
@@ -3,13 +3,15 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App';
 import Login from './Login';
 import SignUp from './Signup';
+import NotFound from './NotFound';
 
 const Router = () => (
   <BrowserRouter>
     <Routes>
-        <Route path="/" element={<App/>} />
-        <Route path="/login" element={<Login/>} />
+        <Route path="/chat" element={<App/>} />
+        <Route path="/" element={<Login/>} />
         <Route path="/signup" element={<SignUp/>} />
+        <Route path="*" element={<NotFound/>} />
     </Routes>
   </BrowserRouter>
 );

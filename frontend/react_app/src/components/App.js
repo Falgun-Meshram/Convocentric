e852c8fd98354f387582e4ded6f4bffeb6f18fbc
Falgun Meshram <falgunmeshram@Falguns-MacBook-Pro.local>
2022-01-30 17:44:23 -0500

    Imported react to allow jest to run

--- frontend/react_app/src/components/App.js
+++ frontend/react_app/src/components/App.js
@@ -1,14 +1,16 @@
-import logo from '../logo.svg';
+// import logo from '../logo.svg';
 import '../css/App.css';
+import React from 'react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>hello</p>
         <a
           className="App-link"
           href="https://reactjs.org"

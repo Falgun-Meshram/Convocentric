2694f8719a78506749d3f3174992b8619e074068
Falgun Meshram <falgunmeshram@Falguns-MacBook-Pro.local>
2022-02-05 17:46:47 -0500

    Added test : "Correct Input Given"

--- frontend/react_app/src/__tests__/Signup.test.js
+++ frontend/react_app/src/__tests__/Signup.test.js
@@ -2,6 +2,7 @@ import { render, fireEvent, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event'
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import App from '../components/App';
 import Signin from '../components/Signin';
 import SignUp from '../components/Signup';
 
@@ -15,20 +16,40 @@ describe("<Signup>", () => {
             </BrowserRouter>
         );
     });
-    it("Input Validation", () => {
+    it("Input Validation: No input given", () => {
         const { container, debug } = render(
             <BrowserRouter>
                 <SignUp />
             </BrowserRouter>
         );
-        // debug();
-        const e = fireEvent.click(screen.getByText("Sign Up"))
-        console.log(`e is  ${e}`);
-        // expect(screen.getByText("Already have an account?")).toBeInTheDocument();
         expect(screen.getByText("Please provide a valid name")).toBeInTheDocument();
         expect(screen.getByText("Please provide a valid email")).toBeInTheDocument();
         expect(screen.getByText("Your password must be atleast 8 characters, and must contain atleast one capital letter, one small letter and one number and one special character")).toBeInTheDocument();
         expect(screen.getByText("Password should match")).toBeInTheDocument();
+    }
+    )
+    it("Input Validation: Correct input given", () => {
+        const { container, debug } = render(
+            <BrowserRouter>
+                <SignUp />
+                <App />
+            </BrowserRouter>
+        );
+        console.log(`container ${container}`);
+        const userNameInput = screen.getByPlaceholderText("Enter an username");
+        const emailInput = screen.getByPlaceholderText("name@example.com");
+        const passwordInput = screen.getByPlaceholderText("Enter a password");
+        const confirmInput = screen.getByPlaceholderText("Confirm password");
+
+        console.log(userNameInput);
+
+        fireEvent.change(userNameInput, { target: { value: "asd" } })
+        fireEvent.change(emailInput, { target: { value: "na@ex.com" } })
+        fireEvent.change(passwordInput, { target: { value: "asdfQW!@34" } })
+        fireEvent.change(confirmInput, { target: { value: "asdfQW!@34" } })
+        const e = fireEvent.click(screen.getByText("Sign Up"))
+        expect(screen.getByText("Profile Data")).toBeInTheDocument();
+
     }
     )
     it("Redirect to Login", () => {
@@ -39,9 +60,7 @@ describe("<Signup>", () => {
             </BrowserRouter>
         );
         const e = fireEvent.click(screen.getByText("Sign In"))
-        // debug()
         expect(screen.getByText("Create an account")).toBeInTheDocument()
-        // expect(screen.getByText(/Create an account/i)).toBeInTheDocument();
 
     }
     )

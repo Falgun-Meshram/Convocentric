0ef8363665f4db6ed19c4e8343783dc1b0645921
Falgun Meshram <falgunmeshram@Falguns-MacBook-Pro.local>
2022-02-22 17:53:55 -0500

    Added msw to mock api calls Added Test case for network error

--- frontend/react_app/src/__tests__/Signup.test.js
+++ frontend/react_app/src/__tests__/Signup.test.js
@@ -1,12 +1,28 @@
-import { render, fireEvent, screen } from '@testing-library/react';
+import { render, fireEvent, screen, waitFor, } from '@testing-library/react';
 import userEvent from '@testing-library/user-event'
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { rest } from 'msw'
+import { setupServer } from 'msw/node'
+
 import App from '../components/App';
 import Signin from '../components/Signin';
 import SignUp from '../components/Signup';
 
+const server = setupServer(
+    rest.post("http://localhost:4001/api/signup", (req, res, ctx) => {
+        return res(
+            ctx.status(200),
+            ctx.json({
+                ok: true,
+                error: ""
+            }))
+    })
+)
 
+beforeAll(() => server.listen())
+afterEach(() => server.resetHandlers())
+afterAll(() => server.close())
 
 describe("<Signup>", () => {
     it("Page should render", () => {
@@ -16,39 +32,66 @@ describe("<Signup>", () => {
             </BrowserRouter>
         );
     });
-    it("Input Validation: No input given", () => {
+    it("Input Validation: No input given", async () => {
         const { container, debug } = render(
             <BrowserRouter>
                 <SignUp />
             </BrowserRouter>
         );
+        const e = userEvent.click(screen.getByText("Sign Up"))
         expect(screen.getByText("Please provide a valid name")).toBeInTheDocument();
         expect(screen.getByText("Please provide a valid email")).toBeInTheDocument();
         expect(screen.getByText("Your password must be atleast 8 characters, and must contain atleast one capital letter, one small letter and one number and one special character")).toBeInTheDocument();
         expect(screen.getByText("Password should match")).toBeInTheDocument();
     }
     )
-    it("Input Validation: Correct input given", () => {
-        const { container, debug } = render(
+    it("Network Error", async () => {
+        server.use(
+            rest.post('http://localhost:4001/api/signup', (req, res, ctx) => {
+                return res(ctx.status(400))
+            }),
+        )
+        const { container, debug, rerender } = render(
             <BrowserRouter>
                 <SignUp />
-                <App />
             </BrowserRouter>
         );
-        console.log(`container ${container}`);
         const userNameInput = screen.getByPlaceholderText("Enter an username");
         const emailInput = screen.getByPlaceholderText("name@example.com");
         const passwordInput = screen.getByPlaceholderText("Enter a password");
         const confirmInput = screen.getByPlaceholderText("Confirm password");
 
-        console.log(userNameInput);
+        userEvent.type(userNameInput, "asd")
+        userEvent.type(emailInput, "na@ex.com")
+        userEvent.type(passwordInput, "asdfQW!@34")
+        userEvent.type(confirmInput, "asdfQW!@34")
 
-        fireEvent.change(userNameInput, { target: { value: "asd" } })
-        fireEvent.change(emailInput, { target: { value: "na@ex.com" } })
-        fireEvent.change(passwordInput, { target: { value: "asdfQW!@34" } })
-        fireEvent.change(confirmInput, { target: { value: "asdfQW!@34" } })
-        const e = fireEvent.click(screen.getByText("Sign Up"))
-        expect(screen.getByText("Profile Data")).toBeInTheDocument();
+        userEvent.click(screen.getByText("Sign Up"))
+
+        const c = await screen.findByText(/Network Error/i);
+        expect(c).toBeInTheDocument();
+
+    })
+    it("Input Validation: Correct input given", async () => {
+        const { container, debug, rerender } = render(
+            <BrowserRouter>
+                <SignUp />
+            </BrowserRouter>
+        );
+        const userNameInput = screen.getByPlaceholderText("Enter an username");
+        const emailInput = screen.getByPlaceholderText("name@example.com");
+        const passwordInput = screen.getByPlaceholderText("Enter a password");
+        const confirmInput = screen.getByPlaceholderText("Confirm password");
+
+        userEvent.type(userNameInput, "asd")
+        userEvent.type(emailInput, "na@ex.com")
+        userEvent.type(passwordInput, "asdfQW!@34")
+        userEvent.type(confirmInput, "asdfQW!@34")
+
+        userEvent.click(screen.getByText("Sign Up"))
+
+        const c = await screen.findByText("Sign Up Successfull");
+        expect(c).toBeInTheDocument();
 
     }
     )
@@ -59,7 +102,7 @@ describe("<Signup>", () => {
                 <Signin />
             </BrowserRouter>
         );
-        const e = fireEvent.click(screen.getByText("Sign In"))
+        const e = userEvent.click(screen.getByText("Sign In"))
         expect(screen.getByText("Create an account")).toBeInTheDocument()
 
     }

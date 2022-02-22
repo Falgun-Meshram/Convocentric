8b5df49f2d0182740d72cd9e46f8a8b23e19403c
Falgun Meshram <falgunmeshram@Falguns-MacBook-Pro.local>
2022-02-22 18:25:37 -0500

    Basic setup to do mock up api calls

--- frontend/react_app/src/__tests__/Signin.test.js
+++ frontend/react_app/src/__tests__/Signin.test.js
@@ -3,9 +3,22 @@ import userEvent from '@testing-library/user-event'
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import App from '../components/App';
+import { rest } from 'msw'
+import { setupServer } from 'msw/node'
+
 import Signin from '../components/Signin';
 import SignUp from '../components/Signup';
 
+const server = setupServer(
+    rest.post("http://localhost:4001/api/login", (req, res, ctx) => {
+        return res(
+            ctx.status(200),
+            ctx.json({
+                ok: true,
+                error: ""
+            }))
+    })
+)
 
 
 describe("<Signin>", () => {
@@ -16,30 +29,31 @@ describe("<Signin>", () => {
             </BrowserRouter>
         );
     });
-    // it("Input Validation: No input given", () => {
-    //     const { container, debug } = render(
-    //         <BrowserRouter>
-    //             <SignUp />
-    //         </BrowserRouter>
-    //     );
-    //     expect(screen.getByText("Please provide a valid name")).toBeInTheDocument();
-    //     expect(screen.getByText("Please provide a valid email")).toBeInTheDocument();
-    //     expect(screen.getByText("Your password must be atleast 8 characters, and must contain atleast one capital letter, one small letter and one number and one special character")).toBeInTheDocument();
-    //     expect(screen.getByText("Password should match")).toBeInTheDocument();
-    // }
-    // )
-    it("Input Validation: Correct input given", () => {
+    it("Input Validation: No input given", () => {
         const { container, debug } = render(
             <BrowserRouter>
                 <SignUp />
+            </BrowserRouter>
+        );
+        expect(screen.getByText("Please provide a valid name")).toBeInTheDocument();
+        expect(screen.getByText("Please provide a valid email")).toBeInTheDocument();
+        expect(screen.getByText("Your password must be atleast 8 characters, and must contain atleast one capital letter, one small letter and one number and one special character")).toBeInTheDocument();
+        expect(screen.getByText("Password should match")).toBeInTheDocument();
+    }
+    )
+    //TODO Add a network error test case
+    it("Input Validation: Correct input given", () => {
+
+        //TODO Add the msw to mock the api calls
+        const { container, debug } = render(
+            <BrowserRouter>
+                <Signin />
                 <App />
             </BrowserRouter>
         );
         console.log(`container ${container}`);
         const userNameInput = screen.getByPlaceholderText("Username");
         const passwordInput = screen.getByPlaceholderText("Password");
-
-        console.log(userNameInput);
 
         fireEvent.change(userNameInput, { target: { value: "asd" } })
         fireEvent.change(passwordInput, { target: { value: "asdfQW!@34" } })

43b4c7c83690ad799f1a653b2627bd96d6233655
Falgun Meshram <fdmeshra@uwaterloo.ca>
2022-04-02 17:52:45 -0400

    Mock local storage setup

--- frontend/react_app/src/setupTests.js
+++ frontend/react_app/src/setupTests.js
@@ -2,4 +2,26 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom'
+// import '@testing-library/jest-dom'
+class LocalStorageMock {
+    constructor() {
+        this.store = {};
+    }
+    clear() {
+        this.store = {};
+    }
+    getItem(key) {
+        return this.store[key] || null;
+    }
+    setItem(key, value) {
+        this.store[key] = value.toString();
+    }
+    removeItem(key) {
+        delete this.store[key];
+    }
+}
+
+const localStorage = new LocalStorageMock();
+
+// jest test environment expects these to exist
+window.localStorage = localStorage

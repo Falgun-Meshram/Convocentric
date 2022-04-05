b7654464d6d115efafa8a124e54f6b1adbe67ce3
ABHISHEK <a29kulka@uwaterloo.ca>
2022-04-04 22:35:59 -0400

    backend test cases added.

--- backend/serializers.py
+++ backend/serializers.py
@@ -1,5 +1,6 @@
+from email import message
 from rest_framework import serializers
-from .models import User, Chat
+from .models import User, Chat, Message
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -10,7 +11,17 @@ class UserSerializer(serializers.ModelSerializer):
         return User.objects.create_user(**validated_data)
 
 
+class MessageSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Message
+        fields = '__all__'
+
 class ChatSerializer(serializers.ModelSerializer):
+    participants = UserSerializer(many=True, read_only=True)
+    message = MessageSerializer(many=True, read_only=True)
+
     class Meta:
         model = Chat
         fields = '__all__'
+
--- backend/tests/test_views.py
+++ backend/tests/test_views.py
@@ -2,10 +2,11 @@ import json
 from rest_framework import status
 from django.test import TestCase, Client
 from django.urls import reverse
-from ..models import User
-from ..serializers import UserSerializer
+from ..models import User, Chat
+from ..serializers import ChatSerializer, UserSerializer
 from django.contrib.auth.hashers import make_password
 from ..utils import get_token
+from django.shortcuts import get_object_or_404
 
 # initialize the APIClient app
 client = Client()
@@ -46,7 +47,7 @@ class GetAllUsers(TestCase):
         # get data from db
         users = User.objects.all()
         serializer = UserSerializer(users, many=True)
-        self.assertEqual(response.data['users'], serializer.data)
+        self.assertEqual(len(response.data['users']), len(serializer.data)-1)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
 
@@ -90,10 +91,16 @@ class CreateNewUserTest(TestCase):
             'password': 'Testing104!',
         }
 
-        self.invalid_payload = {
+        self.invalid_payload_1 = {
             'username': '',
             'email': 'test10@test.com',
             'password': 'Testing105!',
+        }
+
+        self.invalid_payload_2 = {
+            'username': '',
+            'email': 'test10@test.com',
+            'password': '',
         }
 
     def test_create_user(self):
@@ -104,10 +111,18 @@ class CreateNewUserTest(TestCase):
         )
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-    def test_create_invalid_user(self):
+    def test_create_invalid_user_1(self):
         response = client.post(
             reverse('signup'),
-            data=json.dumps(self.invalid_payload),
+            data=json.dumps(self.invalid_payload_1),
+            content_type='application/json'
+        )
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+    
+    def test_create_invalid_user_2(self):
+        response = client.post(
+            reverse('signup'),
+            data=json.dumps(self.invalid_payload_2),
             content_type='application/json'
         )
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
@@ -184,3 +199,298 @@ class DeleteSingleUserTest(TestCase):
         response = client.delete(
             reverse('manage_user', kwargs={'pk': 30}), **self.headers)
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+# ----------------------------------------
+
+class GetAllChats(TestCase):
+    """ Test module for GET all chats API """
+
+    def setUp(self):
+
+        username_list = ['test1', 'test2', 'test3', 'test4']
+        email_address_list = ['test1@test.com', 'test2@test.com', 'test3@test.com', 'test4@test.com']
+        raw_password_list = ['Testing123!', 'Testing456!', 'Testing789!', 'Testing012!']
+        password_list = []
+        for password in raw_password_list:
+            password_obj = make_password(password)
+            password_list.append(password_obj)
+
+        users = []
+
+        # CREATE YOUR USERS
+        for i in range(4):
+            post_data = dict(username=username_list[i], email=email_address_list[i], password=raw_password_list[i])
+            serializer_data = UserSerializer(data=post_data)
+            if serializer_data.is_valid():
+                serializer_data.save()
+                users.append(serializer_data)
+        
+        # CREATE YOUR CHATS
+        for i in range(len(users)-1):
+            chat = Chat()
+            chat.save()
+            sender = User.objects.get(pk=users[i].data['id'])
+            reciever = User.objects.get(pk=users[i].data['id'])
+            chat.participants.add(sender)
+            chat.participants.add(reciever)
+
+        post_data = dict(username='test1', email='test1@test.com', password='Testing123!')
+
+        self.serializer = users[0]
+
+        self.token = get_token(self, client, post_data, self.serializer)
+        self.headers = {'HTTP_AUTHORIZATION': 'Token ' + self.token}
+
+    def test_get_all_chats(self):
+        # get API response
+        response = client.get(reverse('get_all_chats'), **self.headers)
+        # get data from db
+        chats = Chat.objects.all()
+        chat_serializer = ChatSerializer(chats, many=True).data
+        response_chats = response.data['chats']
+        self.assertEqual(chat_serializer, response_chats)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+class GetSingleChatTest(TestCase):
+    """ Test module for GET single chat API """
+
+    def setUp(self):
+
+        username_list = ['test1', 'test2', 'test3', 'test4']
+        email_address_list = ['test1@test.com', 'test2@test.com', 'test3@test.com', 'test4@test.com']
+        raw_password_list = ['Testing123!', 'Testing456!', 'Testing789!', 'Testing012!']
+        password_list = []
+        for password in raw_password_list:
+            password_obj = make_password(password)
+            password_list.append(password_obj)
+
+        users = []
+
+        # CREATE YOUR USERS
+        for i in range(4):
+            post_data = dict(username=username_list[i], email=email_address_list[i], password=raw_password_list[i])
+            serializer_data = UserSerializer(data=post_data)
+            if serializer_data.is_valid():
+                serializer_data.save()
+                users.append(serializer_data)
+        
+        # CREATE YOUR CHATS
+        self.existing_chats = []
+        for i in range(len(users)-1):
+            chat = Chat()
+            chat.save()
+            self.existing_chats.append(chat.id)
+            sender = User.objects.get(pk=users[i].data['id'])
+            reciever = User.objects.get(pk=users[i].data['id'])
+            chat.participants.add(sender)
+            chat.participants.add(reciever)
+
+        post_data = dict(username='test1', email='test1@test.com', password='Testing123!')
+
+        self.serializer = users[0]
+
+        self.token = get_token(self, client, post_data, self.serializer)
+        self.headers = {'HTTP_AUTHORIZATION': 'Token ' + self.token}
+
+    def test_get_valid_chat(self):
+        response = client.get(
+            reverse('get_chat', kwargs={'pk': self.existing_chats[0]}), **self.headers)
+        chat = Chat.objects.get(pk=self.existing_chats[0])
+        self.assertEqual(chat.id, self.existing_chats[0])
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_invalid_chat(self):
+        response = client.get(
+            reverse('get_chat', kwargs={'pk': 50}), **self.headers)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+
+class CreateNewChatTest(TestCase):
+    """ Test module for inserting a new user """
+    
+    def setUp(self):
+
+        username_list = ['test1', 'test2', 'test3', 'test4']
+        email_address_list = ['test1@test.com', 'test2@test.com', 'test3@test.com', 'test4@test.com']
+        raw_password_list = ['Testing123!', 'Testing456!', 'Testing789!', 'Testing012!']
+        password_list = []
+        for password in raw_password_list:
+            password_obj = make_password(password)
+            password_list.append(password_obj)
+
+        users = []
+
+        # CREATE YOUR USERS
+        existing_users = []
+        for i in range(4):
+            post_data = dict(username=username_list[i], email=email_address_list[i], password=raw_password_list[i])
+            serializer_data = UserSerializer(data=post_data)
+            if serializer_data.is_valid():
+                serializer_data.save()
+                existing_users.append(serializer_data.data['id'])
+                users.append(serializer_data)
+
+        post_data = dict(username='test1', email='test1@test.com', password='Testing123!')
+
+        self.serializer = users[0]
+
+        self.token = get_token(self, client, post_data, self.serializer)
+        self.headers = {'HTTP_AUTHORIZATION': 'Token ' + self.token}
+
+        self.valid_payload = {
+            'senderId': existing_users[0],
+            'recieverId': existing_users[1],
+        }
+
+        self.invalid_payload_1 = {
+            'senderId': existing_users[0],
+            'recieverId': None,
+        }
+
+        self.invalid_payload_2 = {
+            'senderId': None,
+            'recieverId': existing_users[0],
+        }
+
+    def test_create_chat(self):
+        response = client.post(
+            reverse('create_chat'),
+            data=json.dumps(self.valid_payload),
+            content_type='application/json', **self.headers
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_create_invalid_chat_1(self):
+        response = client.post(
+            reverse('create_chat'),
+            data=json.dumps(self.invalid_payload_1),
+            content_type='application/json', **self.headers
+        )
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+    
+    def test_create_invalid_chat_2(self):
+        response = client.post(
+            reverse('create_chat'),
+            data=json.dumps(self.invalid_payload_2),
+            content_type='application/json', **self.headers
+        )
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+class DeleteSingleChatTest(TestCase):
+    """ Test module for deleting an existing user record """
+
+    def setUp(self):
+        
+        username_list = ['test1', 'test2', 'test3', 'test4']
+        email_address_list = ['test1@test.com', 'test2@test.com', 'test3@test.com', 'test4@test.com']
+        raw_password_list = ['Testing123!', 'Testing456!', 'Testing789!', 'Testing012!']
+        password_list = []
+        for password in raw_password_list:
+            password_obj = make_password(password)
+            password_list.append(password_obj)
+
+        users = []
+
+        # CREATE YOUR USERS
+        for i in range(4):
+            post_data = dict(username=username_list[i], email=email_address_list[i], password=raw_password_list[i])
+            serializer_data = UserSerializer(data=post_data)
+            if serializer_data.is_valid():
+                serializer_data.save()
+                users.append(serializer_data)
+        
+        # CREATE YOUR CHATS
+        self.existing_chats = []
+        for i in range(len(users)-1):
+            chat = Chat()
+            chat.save()
+            self.existing_chats.append(chat.id)
+            sender = User.objects.get(pk=users[i].data['id'])
+            reciever = User.objects.get(pk=users[i].data['id'])
+            chat.participants.add(sender)
+            chat.participants.add(reciever)
+
+        post_data = dict(username='test1', email='test1@test.com', password='Testing123!')
+
+        self.serializer = users[0]
+
+        self.token = get_token(self, client, post_data, self.serializer)
+        self.headers = {'HTTP_AUTHORIZATION': 'Token ' + self.token}
+            
+        self.chat_id = self.existing_chats[0]
+
+        self.token = get_token(self, client, post_data, self.serializer)
+        
+        self.headers = {'HTTP_AUTHORIZATION': 'Token ' + self.token}
+
+    def test_valid_delete_chat(self):
+        
+        response = client.delete(
+            reverse('delete_chat', kwargs={'pk': self.chat_id }), **self.headers)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_invalid_delete_user(self):
+        response = client.delete(
+            reverse('delete_chat', kwargs={'pk': 50 }), **self.headers)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+
+class DeleteAllChatTest(TestCase):
+
+    def setUp(self):
+        
+        username_list = ['test1', 'test2', 'test3', 'test4']
+        email_address_list = ['test1@test.com', 'test2@test.com', 'test3@test.com', 'test4@test.com']
+        raw_password_list = ['Testing123!', 'Testing456!', 'Testing789!', 'Testing012!']
+        password_list = []
+        for password in raw_password_list:
+            password_obj = make_password(password)
+            password_list.append(password_obj)
+
+        users = []
+
+        # CREATE YOUR USERS
+        for i in range(4):
+            post_data = dict(username=username_list[i], email=email_address_list[i], password=raw_password_list[i])
+            serializer_data = UserSerializer(data=post_data)
+            if serializer_data.is_valid():
+                serializer_data.save()
+                users.append(serializer_data)
+        
+        # CREATE YOUR CHATS
+        self.existing_chats = []
+        for i in range(len(users)-1):
+            chat = Chat()
+            chat.save()
+            self.existing_chats.append(chat.id)
+            sender = User.objects.get(pk=users[i].data['id'])
+            reciever = User.objects.get(pk=users[i].data['id'])
+            chat.participants.add(sender)
+            chat.participants.add(reciever)
+
+        post_data = dict(username='test1', email='test1@test.com', password='Testing123!')
+
+        self.serializer = users[0]
+
+        self.token = get_token(self, client, post_data, self.serializer)
+
+        self.headers = {'HTTP_AUTHORIZATION': 'Token ' + self.token}
+
+        self.chat_id = self.existing_chats[0]
+
+        self.token = get_token(self, client, post_data, self.serializer)
+        
+        self.headers = {'HTTP_AUTHORIZATION': 'Token ' + self.token}
+
+        self.token = get_token(self, client, post_data, self.serializer)
+        
+        self.headers = {'HTTP_AUTHORIZATION': 'Token ' + self.token}
+    
+    def test_delete_all_chats(self):
+        # get API response
+        response = client.delete(reverse('delete_all_chats'), **self.headers)
+        # get data from db
+        chats = Chat.objects.all()
+        chats_len = len(chats)
+        self.assertEqual(chats_len, 0)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
